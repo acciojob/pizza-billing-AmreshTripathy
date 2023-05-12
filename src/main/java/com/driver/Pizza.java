17e33a294e330e0e73163855193b68504734d422
AmreshTripathy <amreshtripathy184@gmail.com>
2023-05-12 23:31:09 +0530

    Pizza Push-10

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -17,10 +17,8 @@ public class Pizza {
         this.bill = "";
         if (this.isVeg) {
             this.price += 300;
-            this.bill += "Base Price Of The Pizza: " + 300 + "\n";
         } else {
             this.price += 400;
-            this.bill += "Base Price Of The Pizza: " + 400 + "\n";
         }
 
         this.isExtraCheeseAdded = false;
@@ -37,7 +35,6 @@ public class Pizza {
         // your code goes here
         if (!this.isExtraCheeseAdded && !this.isExtraToppingsAddedVeg && !isExtraToppingsAddedNonVeg) {
             this.isExtraCheeseAdded = true;
-            this.bill += "Extra Cheese Added: " + 80 + "\n";
             this.price += 80;
         }
     }
@@ -48,12 +45,10 @@ public class Pizza {
             this.isExtraToppingsAddedVeg = true;
             this.isExtraCheeseAdded = true;
             this.price += 70;
-            this.bill += "Extra Toppings Added: " + 70 + "\n";
-        } else if (!this.isExtraToppingsAddedNonVeg && !this.isVeg){
+        } else if (!this.isExtraToppingsAddedNonVeg && !this.isVeg) {
             this.isExtraToppingsAddedNonVeg = true;
             this.isExtraCheeseAdded = true;
             this.price += 120;
-            this.bill += "Extra Toppings Added: " + 120 + "\n";
         }
     }
 
@@ -61,13 +56,29 @@ public class Pizza {
         // your code goes here
         if (!this.isTakewayAdded) {
             this.isTakewayAdded = true;
-            this.bill += "Paperbag Added: " + 20 + "\n";
             this.price += 20;
         }
     }
 
     public String getBill() {
         // your code goes here
+        if (this.isVeg) {
+            this.bill += "Base Price Of The Pizza: " + 300 + "\n";
+        } else {
+            this.bill += "Base Price Of The Pizza: " + 400 + "\n";
+        }
+
+        if (this.isExtraCheeseAdded)
+            this.bill += "Extra Cheese Added: " + 80 + "\n";
+
+        if (this.isVeg && this.isExtraToppingsAddedVeg)
+            this.bill += "Extra Toppings Added: " + 70 + "\n";
+        else if (!this.isVeg && this.isExtraToppingsAddedNonVeg)
+            this.bill += "Extra Toppings Added: " + 120 + "\n";
+
+        if (this.isTakewayAdded)
+            this.bill += "Paperbag Added: " + 20 + "\n";
+
         this.bill += "Total Price: " + this.price + "\n";
         return this.bill;
     }
